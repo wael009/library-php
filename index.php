1b696c1914a2f421d40bec15df5d28a75695724a
wael <wael_sy@hotmail.com>
2020-02-01 22:26:06 +0000

    Modify in front end

--- index.php
+++ index.php
@@ -5,27 +5,27 @@ session_start();
 // ------Do Not forget to add the alert here if the username or password was not correct -----------
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta http-equiv="X-UA-Copmatible" content="ie-edge">
 	<title>Login Page</title>
-    <link rel="stylesheet" href="bootstrap.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   	<link rel="stylesheet" href="stylesheet/style.css" />
 </head>
 <body>
 
 	<div class="login-clean">
         <form action="login.php" method="POST">
-            <h2 class="sr-only">Login Form</h2>
-            <div class="illustration"><i class="icon ion-ios-navigate"></i></div>
+            <h2 class="login-form">Log in to admin account</h2>
             <div class="form-group"><input class="form-control" type="text" name="user" placeholder="Username" autofocus required /></div>
             <div class="form-group"><input class="form-control" type="password" name="pass" placeholder="Password" required /></div>
             <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="submit">Log in</button></div>
-				<a href="#" class="forgot">Forgot your email or password?</a>       
-		</form>
+
+            </form>
 
     </div>
 
