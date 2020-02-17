9dc89382d10bb1b5d59eb501503898eda661b050
wael <wael_sy@hotmail.com>
2020-02-17 23:31:56 +0000

    modified in updare and readOnce functions

--- create.php
+++ create.php
@@ -9,6 +9,9 @@ if(isset($_POST['submit']))
   $title = $_POST['title'];
   $author = $_POST['author'];
   $publisher = $_POST['publisher'];
+  $subject = $_POST['subject'];
+  $description = $_POST['description'];
+  $isbn = $_POST['isbn'];
   $image = $_POST['image'];
 
 
@@ -16,15 +19,20 @@ $fields = [
   'title'=>$title,
   'author'=>$author,
   'publisher'=>$publisher,
-  'image'=>$image
+  'subject'=>$subject,
+  'description'=>$description,
+  'image'=>$image,
+  'isbn'=>$isbn,
+
 ];
+
 
 $createObj = new bookClass();
 $createObj->create($fields);
 
 // if(isset($POST['submit-cover']))
 
-$createObj->uploadPhoto();
+//$createObj->uploadPhoto();
 
 
 
@@ -45,36 +53,50 @@ include "layout/header.php";
 
 
         <form action="" method="post">
+
   <div class="form-group">
     <label for="title">Title</label>
     <input type="text" class="form-control" name="title" placeholder="Enter book title">
   </div>
+
   <div class="form-group">
     <label for="author">Author</label>
     <input type="text" class="form-control" name="author" placeholder="Author name">
   </div>
+
   <div class="form-group">
     <label for="publisher">Publisher</label>
     <input type="text" class="form-control" name="publisher" placeholder="Publisher">
+  </div>
+
+  <div class="form-group">
+    <label for="subject">Subject</label>
+    <input type="text" class="form-control" name="subject" placeholder="subject">
+  </div>
+
+  <div class="form-group">
+    <label for="description">Description</label>
+    <input type="text" class="form-control" name="description" placeholder="description">
+  </div>
+
+  <div class="form-group">
+    <label for="isbn">ISBN</label>
+    <input type="text" class="form-control" name="isbn" placeholder="isbn">
   </div>
   
   <div class="form-group">
     <label for="image">Select book Cover</label>
     <input type="file" class="btn btn-outline-dark" name="image" placeholder="Book cover">
   </div>
+  
   <br/> 
     <input type="submit" name="submit" class="btn btn-primary btn-lg">
-
-
-
 
 
 
   <br/> 
 </form>
 
-
-        
       </div>
      </div>
     </div>
--- update.php
+++ update.php
@@ -23,6 +23,9 @@ if(isset($_POST['submit']))
   $title = $_POST['title'];
   $author = $_POST['author'];
   $publisher = $_POST['publisher'];
+  $subject = $_POST['subject'];
+  $description = $_POST['description'];
+  $isbn = $_POST['isbn'];
   $image = $_POST['image'];
 
 
@@ -30,7 +33,11 @@ $fields = [
   'title'=>$title,
   'author'=>$author,
   'publisher'=>$publisher,
-  'image'=>$image
+  'subject'=>$subject,
+  'description'=>$description,
+  'image'=>$image,
+  'isbn'=>$isbn,
+
 ];
 
 $id = $_POST['id'];
@@ -42,14 +49,10 @@ $updateObj->update($fields, $id);
 
 ?>
 
-
-
-
-
 <div class="container mt-4">
+<div class="main">
   <div class="row">
-    <div class="col-lg-12">
-      <div class="jumbotron">
+    <div class="col-lg-8">
         <h4 class="mb-4">Update Book</h4>
 
         <form action="" method="post">
@@ -63,23 +66,46 @@ $updateObj->update($fields, $id);
     <input type="text" class="form-control" name="author" placeholder="Author name" value="<?php echo $result['author'];?>">
   </div>
   <div class="form-group">
-    <label for="publisher">publisher</label>
+    <label for="publisher">Publisher</label>
     <input type="text" class="form-control" name="publisher" placeholder="Publisher" value="<?php echo $result['publisher'];?>">
   </div>
+
+  <div class="form-group">
+    <label for="subject">Subject</label>
+    <input type="text" class="form-control" name="subject" placeholder="subject" value="<?php echo $result['subject'];?>">
+  </div>
+
+  <div class="form-group">
+    <label for="description">Description</label>
+    <input type="text" class="form-control" name="description" placeholder="description" value="<?php echo $result['description'];?>">
+  </div>
+
+  <div class="form-group">
+    <label for="isbn">ISBN</label>
+    <input type="text" class="form-control" name="isbn" placeholder="Input ISBN number please" value="<?php echo $result['isbn'];?>">
+  </div>
+
   <div class="form-group">
     <label for="cover">Book Cover</label>
     <input type="file" class="form-control-file" name="image" placeholder="image" value="<?php echo $result['image'];?>">
   </div>
   <br/> 
 
+  
+
   <input type="submit" name="submit" class="btn btn-primary">
 </form>
 
 
         
-      </div>
      </div>
+     <div class="col-lg-4">
+    <tr><br/><br/><br/>
+        <td><img src="upload/<?php echo $result['image'];?>" width="400" height="490" alt="Card image cap"></td>
+    </tr>
+</div>
     </div>
+  </div>
 </div>
 
 
